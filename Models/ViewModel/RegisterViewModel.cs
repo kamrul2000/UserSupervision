75ac750f4e13e1906a2017997422a5f6a0e19b5b
mdrezaulkarim38 <mdrezaulkarim31295@gmail.com>
2025-04-22 14:18:47 +0600

    Now Need to change the ui in list

--- Models/ViewModel/RegisterViewModel.cs
+++ Models/ViewModel/RegisterViewModel.cs
@@ -5,19 +5,10 @@ namespace UserSupervision.Models.ViewModel;
 public class RegisterViewModel
 {
     [Required]
-    public string FullName { get; set; }
-
+    public string? FullName { get; set; }
     [Required, EmailAddress]
-    public string Email { get; set; }
-
+    public string? Email { get; set; }
     [Required]
-    public string Password { get; set; }
-
-   
-    public string Mobile { get; set; }
-
-
-    public string Address { get; set; }
-
-   
+    public string? Password { get; set; }
+    public int? Mobile { get; set; }
 }
